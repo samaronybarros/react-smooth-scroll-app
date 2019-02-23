dcb89968c60ac190da92d86505c8cd22f7fed5a1
Sam Barros <samarony.barros@gmail.com>
2019-02-23 12:10:21 +0100

    Creates fake-data and includes logo

---
+++ src/Components/index.js
@@ -0,0 +1,8 @@
+import Logo from './Logo'
+import NavBar from './NavBar'
+import Section from './Section'
+import ListSection from './ListSection'
+
+export default NavBar
+
+export { Logo, NavBar, Section, ListSection }
---
+++ src/Constants/fakeList.js
@@ -0,0 +1,48 @@
+import loremIpsum from './LoremIpsum'
+
+const dummyList = [
+  {
+    id: 'section1',
+    title: 'Section 1',
+    text: loremIpsum,
+    dark: true,
+    duration: 500
+  },
+  {
+    id: 'section2',
+    title: 'Section 2',
+    text: loremIpsum,
+    dark: false,
+    duration: 500
+  },
+  {
+    id: 'section3',
+    title: 'Section 3',
+    text: loremIpsum,
+    dark: true,
+    duration: 500
+  },
+  {
+    id: 'section4',
+    title: 'Section 4',
+    text: loremIpsum,
+    dark: false,
+    duration: 500
+  },
+  {
+    id: 'section5',
+    title: 'Section 5',
+    text: loremIpsum,
+    dark: true,
+    duration: 500
+  },
+  {
+    id: 'section6',
+    title: 'Section 6',
+    text: loremIpsum,
+    dark: false,
+    duration: 500
+  }
+]
+
+export default dummyList
