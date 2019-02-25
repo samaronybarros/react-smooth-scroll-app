5b7699b75eb9bffb7547d7f4b7c3551e4b9ba92e
Sam Barros <samarony.barros@gmail.com>
2019-02-25 16:06:33 +0100

    Creates files for default css element

---
+++ src/Style/index.js
@@ -0,0 +1,20 @@
+export default {
+  logoDefaultWidth: 60,
+  logoDefaultHeight: 60,
+
+  navShadowColor: 'rgba(0, 0, 0, 0.2)',
+  navBgColor: '#FAFAFA',
+  navTextColor: '#b0005b',
+
+  darkBgColor: '#b0005b',
+  darkTextColor: '#FFF',
+
+  lightBgColor: '#FFF',
+  lightTextColor: '#333',
+
+  hoverTextColor: '#333',
+  hoverShadowColor: '#23a8a4',
+
+  sectionTitleAlign: 'center',
+  sectionParagraphAlign: 'justify'
+}
